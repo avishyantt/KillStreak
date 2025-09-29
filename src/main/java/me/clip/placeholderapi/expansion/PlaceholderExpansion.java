b719187b5f6780b16495bc3272143c084174a728
Avishyant <avishyantaryan@gmail.com>
2025-09-29 11:50:52 +0530

    Update PlaceholderExpansion.java

--- src/main/java/me/clip/placeholderapi/expansion/PlaceholderExpansion.java
+++ src/main/java/me/clip/placeholderapi/expansion/PlaceholderExpansion.java
@@ -1,13 +1,21 @@
 package me.clip.placeholderapi.expansion;
 
+import org.bukkit.entity.Player;
+
 public abstract class PlaceholderExpansion {
+
     public abstract boolean canRegister();
+
     public abstract String getIdentifier();
+
     public abstract String getAuthor();
+
     public abstract String getVersion();
-    public abstract String onPlaceholderRequest(Object player, String identifier);
+
+    // Must match exactly what your KillStreakPlaceholder overrides
+    public abstract String onPlaceholderRequest(Player player, String identifier);
 
     public boolean register() {
-        return true; // stub
+        return true; // fake registration for compile only
     }
 }
